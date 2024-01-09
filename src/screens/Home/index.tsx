028d1092987f51300c3c8e88f7e780ede0d9319d
Adson <adson.oliveira.inf@gmail.com>
2024-01-08 22:10:39 -0300

    Componente rodapé

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { Button } from "../../components/Button";
 import { Container, Scroll,ButtonContainer } from "./styles";
+import { Baseboard } from '../../components/Baseboard';
 
 export function Home() {
   return (
     <Container>
       <StatusBar style="light" />
+      {/*<Baseboard/>*/}
       <Scroll>
         {/* Conteúdo da tela */}
       </Scroll>
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,9 +1,11 @@
+import { Platform } from "react-native";
 import styled from "styled-components/native";
 
 export const Container = styled.SafeAreaView`
   flex: 1;
   background-color: #F6F8FC;
   padding: 0 16px;
+  padding-top: ${Platform.OS === "android" ? 56 : 0}px;
   justify-content: center;
   align-items: center;
 `;
