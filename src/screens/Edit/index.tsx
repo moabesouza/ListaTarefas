2f6906fc277a90290a7827f74e0c033a78544420
Moabe de jesus souza <moabe.2009@hotmail.com>
2024-01-12 21:20:13 -0300

    criação da tela edit

---
+++ src/screens/Edit/index.tsx
@@ -0,0 +1,62 @@
+
+import React, { useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { Container, Scroll, ContentContainer, FormContainer, InputLabel, ButtonContainer } from './styles';
+import { Input } from '../../components/Input';
+import { Textarea } from '../../components/Textarea';
+import { Select } from '../../components/Select';
+import { Button } from '../../components/Button';
+import { Header } from '../../components/Header';
+import { TipoData } from '../../Data/tipo';
+import { StatusData } from '../../Data/status';
+
+export function Edit() {
+  const [selectedStatus, setSelectedStatus] = useState('');
+  const [selectedTipo, setSelectedTipo] = useState('');
+
+  return (
+    <Container>
+      <StatusBar translucent backgroundColor="transparent" />
+      <Header title="Editar Tarefa"/>
+      <Scroll showsVerticalScrollIndicator={false}>
+        <ContentContainer>
+          <FormContainer>
+            <InputLabel>Tarefa</InputLabel>
+            <Input placeholder="Digite a tarefa"/>
+          </FormContainer>
+
+          <FormContainer>
+            <InputLabel>Status</InputLabel>
+            <Select
+              options={[...StatusData]}
+              labelExtractor={(item) => item.descricao}
+              valueExtractor={(item) => item.id.toString()}
+              selectedValue={selectedStatus}
+              onValueChange={(itemValue) => setSelectedStatus(itemValue)}
+            />
+          </FormContainer>
+
+          <FormContainer>
+            <InputLabel>Tipo</InputLabel>
+            <Select
+              options={[...TipoData]}
+              labelExtractor={(item) => item.descricao}
+              valueExtractor={(item) => item.id.toString()}
+              selectedValue={selectedTipo}
+              onValueChange={(itemValue) => setSelectedTipo(itemValue)}
+            />
+          </FormContainer>
+
+          <FormContainer>
+            <InputLabel>Descrição</InputLabel>
+            <Textarea placeholder="Digite a descrição"/>
+          </FormContainer>
+        </ContentContainer>
+
+        <ButtonContainer>
+          <Button />
+        </ButtonContainer>
+      </Scroll>
+    </Container>
+  );
+}
---
+++ src/screens/Edit/styles.ts
@@ -0,0 +1,34 @@
+import styled from "styled-components/native";
+
+export const Container = styled.SafeAreaView`
+  flex: 1;
+  background-color: #F6F8FC;
+`;
+
+export const Scroll = styled.ScrollView`
+  flex: 1;
+  padding: 16px;
+`;
+
+export const FormContainer = styled.View`
+  margin-bottom: 16px;
+`;
+
+export const InputLabel = styled.Text`
+  font-size: 16px;
+  color: #4E8D7C;
+  margin-bottom: 8px;
+  font-weight: bold;
+  font-family: Poppins;
+`;
+
+export const ButtonContainer = styled.View`
+  align-items: center;
+  margin-bottom: 16px;
+`;
+
+export const ContentContainer = styled.View`
+  flex: 1;
+  padding: 16px;
+`;
+
