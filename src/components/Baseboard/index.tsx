9941c5bf32ebbf7d8817d88b20e9f500de5c9f39
Moabe de jesus souza <moabe.2009@hotmail.com>
2024-01-16 21:32:21 -0300

    iplementação das navegaçõe entre a tela home para o cadastro de tarefa

--- src/components/Baseboard/index.tsx
+++ src/components/Baseboard/index.tsx
@@ -2,14 +2,26 @@ import { Text } from "react-native";
 import { AntDesign } from '@expo/vector-icons';
 import { ButtonRight, ButtonLeft, ButtonCenter, MainFooter, SecondaryFooter } from "./styles";
 
+import { useNavigation } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+
+type RootStackParamList = {
+  Home: undefined;
+  Create: undefined; 
+};
+
+type NavigationProp = NativeStackNavigationProp<RootStackParamList, 'Create'>;
+
 export function Baseboard() {
+  const navigation = useNavigation<NavigationProp>();
+
   return (
     <MainFooter>
-      <ButtonCenter>
+      <ButtonCenter onPress={() => navigation.navigate('Create')}>
         <AntDesign name="pluscircle" size={60} color="#646FD4" />
       </ButtonCenter>
       <SecondaryFooter>
-        <ButtonLeft>
+        <ButtonLeft >
           <AntDesign name="dashboard" size={25} color="#646FD4" />
           <Text style={{ color: '#646FD4', marginTop: 5 }}>Dashboard</Text>
         </ButtonLeft>
