66e94c40dd2d4c82051ced098f88bd6c72ece515
Moabe de jesus souza <moabe.2009@hotmail.com>
2024-01-26 00:16:21 -0300

    funcionalidade do grafico

--- src/components/Graph/index.tsx
+++ src/components/Graph/index.tsx
@@ -1,15 +1,55 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View } from 'react-native';
 import { PieChart } from 'react-native-chart-kit';
-import { Title } from "./styles";
+import { Title } from './styles'; 
+import { useTaskContext } from '../../context/taskContext'; 
+import { getStatusColor, getStatusDescription, getTipoDescription } from '../../utils/statusUtils';
+import { statusEnum, tipoEnum } from '../../Enum/enums';
+
+interface ChartData {
+  name: string;
+  population: number;
+  color: string;
+  legendFontColor: string;
+  legendFontSize: number;
+}
 
 export function Graph() {
-  const chartData = [
-    { name: "Em Aberto", population: 70, color: "#FBA13A", legendFontColor: "#333", legendFontSize: 12 },
-    { name: "Cancelado", population: 15, color: "#FF004B", legendFontColor: "#333", legendFontSize: 12 },
-    { name: "Pausado", population: 15, color: "#9747FF", legendFontColor: "#333", legendFontSize: 12 },
-  ];
+  const { tasks } = useTaskContext();
+  const [chartData, setChartData] = useState<ChartData[]>([]);
 
+  useEffect(() => {
+    const calculateStatusDistribution = () => {
+      const statusCounts: Record<string, number> = {};
+      const statusDescriptions: Record<string, string> = {};
+  
+      tasks.forEach((task) => {
+        const status = task.status;
+        const statusDescription = getStatusDescription(status as statusEnum);
+  
+        if (statusCounts[status]) {
+          statusCounts[status]++;
+        } else {
+          statusCounts[status] = 1;
+        }
+  
+        statusDescriptions[status] = statusDescription;
+      });
+  
+      const chartData: ChartData[] = Object.entries(statusCounts).map(([status, count]) => ({
+        name: statusDescriptions[status],
+        population: count,
+        color: getStatusColor(status as statusEnum), 
+        legendFontColor: '#333',
+        legendFontSize: 12,
+      }));
+  
+      setChartData(chartData);
+    };
+  
+    calculateStatusDistribution();
+  }, [tasks]);
+  
   const containerStyle = {
     marginLeft: 10,
     marginRight: 10,
@@ -17,8 +57,8 @@ export function Graph() {
     padding: 10,
     borderRadius: 25,
     marginTop: 20,
-    borderBottomWidth: 1,  
-    borderBottomColor: '#ddd',  
+    borderBottomWidth: 1,
+    borderBottomColor: '#ddd',
   };
 
   return (
@@ -29,8 +69,8 @@ export function Graph() {
         width={300}
         height={150}
         chartConfig={{
-          backgroundGradientFrom: "#1E2923",
-          backgroundGradientTo: "#08130D",
+          backgroundGradientFrom: '#1E2923',
+          backgroundGradientTo: '#08130D',
           color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
         }}
         accessor="population"
